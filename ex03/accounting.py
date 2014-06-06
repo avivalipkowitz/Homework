80702ca2608688285cf9400e4a07eed93db4c00c
Hackbright Student <info@hackbrightacademy.com>
2014-06-06 16:09:42 -0700

    Ex03: Made all corrections to HW03

--- ex03/accounting.py
+++ ex03/accounting.py
@@ -1,36 +1,34 @@
-def main():
-    
+melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
+melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
 
-    print "******************************************"
-    
+def count_the_melons(): 
     f = open("orders_by_type.csv")
-    
-    melon_tallies = {"Musk": 0, "Hybrid": 0, "Watermelon": 0, "Winter": 0}
     
     for line in f:
         data = line.split(",")
         melon_type = data[1]
         melon_count = int(data[2])
         melon_tallies[melon_type] += melon_count
-    f.close()
 
+    return melon_tallies
     
 
-    melon_prices = { "Musk": 1.15, "Hybrid": 1.30, "Watermelon": 1.75, "Winter": 4.00 }
+def total_the_revenues():
+    f = open("orders_by_type.csv")
     total_revenue = 0
-    
+        
     for melon_type in melon_tallies:
         price = melon_prices[melon_type]
         revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
-        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
-    
-    print "******************************************"
-    
+    f.close()
+    print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+
+def compare_sales_methods():
+
     f = open("orders_with_sales.csv")
-    
+        
     saletype = [0, 0]
-    
     
     for line in f:
         data = line.split(",")
@@ -49,8 +47,15 @@ def main():
         print "Guess there's some value to those salespeople after all."
     else:
         print "Time to fire the sales team! Online sales rule all!"
+
+    f.close()
+
+def main():
     
-    print "******************************************"
+    count_the_melons()
+    total_the_revenues()
+    compare_sales_methods()
+    
 
 
 if __name__ == "__main__":
