01c459d0797e2b21772ceaeaca38f62e5b22168c
Hackbright Student <info@hackbrightacademy.com>
2014-06-06 17:37:15 -0700

    Skills1: Got halfway through, up to halvsies operation

--- skills1/skills1.py
+++ skills1/skills1.py
@@ -1,24 +1,62 @@
 # Things you should be able to do.
 
+num_list = [1,2,3,4,5,6,7,8,9,10,11,12]
+animals = ['cat', 'dog', 'turtle', 'elephant', 'hippo']
+
+
+
 # Write a function that takes a list and returns a new list with only the odd numbers.
 def all_odd(some_list):
-    return []
+    
+    return some_list[::2]
+
+
+odd_list = all_odd(num_list)
+print odd_list
+
+odd_list = all_odd(animals)
+print odd_list
 
 # Write a function that takes a list and returns a new list with only the even numbers.
 def all_even(some_list):
-    return []
+     return some_list[1::2]
+
+even_list = all_even(num_list)
+print even_list
+
+even_list = all_even(animals)
+print even_list
+
 
 # Write a function that takes a list of strings and a new list with all strings of length 4 or greater.
 def long_words(word_list):
-    return []
+    long_words_list = []
+    
+    for word in word_list:
+        word_length = len(word)
+        if word_length >= 4:
+            long_words_list.append(word)
+    return long_words_list
+
+long_words_list = long_words(animals)
+print long_words_list
 
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(some_list):
-    return None
+    some_list.sort()
+    return some_list[0]
+     
+smallest_int = smallest(num_list)
+print smallest_int
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(some_list):
-    return None
+    some_list.sort()
+    return some_list[-1]
+
+biggest_int = largest(num_list)
+print biggest_int
+     
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(some_list):
