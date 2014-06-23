9eb0e0fe8c6733f44fad44964d237b694b6b8deb
Aviva <avivalipkowitz@gmail.com>
2014-06-22 22:48:07 -0700

    Finished skills2, made some progress on roman_num_calc, but still working on getting iv to be 4

--- random_other_exercises/roman_num_calc.py
+++ random_other_exercises/roman_num_calc.py
@@ -1,45 +1,83 @@
 # exercise that supposedly was a twitter interview question
 # build a roman numerals calculator
 
-# read in the first roman numeral
-roman_num = raw_input("Whhich number would you like to convert? ")
 
-char_list = []
+def main():
 
-# split the roman numeral up by character and add each to a list
-# list is necessary because order is important
-for char in roman_num:
-	char.split()
-	char_list.append(char)
-	print char_list
+	# read in the first roman numeral
+	roman_num = raw_input("Which number would you like to convert? ")
 
-# # convert roman numerals to arabic numerals
-# for char in char_list:
-# 	if char == "i":
-# 		char = 1
-# 	if char == "v":
-# 		char = 5
-# 	if char == "x":
-# 		char = 10
-# 	else:
-# 		print "I can't count higher than 40, pick a smaller number."
-# 		break
-# 	print char_list
+	char_list = []
 
-char_list = [10, 1, 5]
+	# split the roman numeral up by character and add each to a list
+	# list is necessary because order is important
 
-# add up the value of list to get arabic numerals. check if 1 is in there before other numbers
-if len(char_list) == 1:
-	arab_num = char_list[0]
+	def split_roman(roman_num):
+		for char in roman_num:
+			char_list.append(char)
+		print char_list
+		return char_list
 
-if len(char_list) == 2 and char_list[0] == 1:
-	arab_num = char_list[1] - char_list[0]
-else:
-	arab_num = sum(char_list)
+	def convert_roman_to_arabic(char_list):
+		# convert roman numerals to arabic numerals
+		for i in range(0, len(char_list)):
+			if char_list[i] == "i":
+				char_list[i] = 1
 
-if len(char_list) == 3 and char_list[1] ==1:
-	arab_num = char_list[0] + char_list[2] - char_list[1]
-else: 
-	arab_num = sum(char_list)
+			elif char_list[i] == "v":
+				char_list[i] = 5
 
-print arab_num
+			elif char_list[i] == "x":
+				char_list[i] = 10
+			
+			else:
+				print "I can't count higher than 40, pick a smaller number."
+				break
+		print("char_list is: %r ") % char_list
+		return char_list
+
+# char_list = [10, 1, 5]
+
+	def sum_of_arab_nums(char_list):
+		sum_list = []
+		# add up the value of list to get arabic numerals. check if 1 is in there before other numbers
+		if char_list[-2] == 1:
+			for i in range(0, -2):
+				sum_list.append(char_list[i])
+			arab_num = sum(sum_list) + char_list[-1] - char_list[-2]
+
+		else: 
+			arab_num = sum(char_list)
+
+		return arab_num
+
+
+
+		# if len(char_list) == 1:
+		# 	arab_num = char_list[0]
+
+
+		# if len(char_list) == 1:
+		# 	arab_num = char_list[0]
+
+		# if len(char_list) == 2 and char_list[0] == 1:
+		# 	arab_num = char_list[1] - char_list[0]
+		# else:
+		# 	arab_num = sum(char_list)
+
+		# if len(char_list) == 3 and char_list[1] ==1:
+		# 	arab_num = char_list[0] + char_list[2] - char_list[1]
+		# else: 
+		# 	arab_num = sum(char_list)
+
+
+		# print arab_num
+		# return arab_num
+
+	arab_total = sum_of_arab_nums(convert_roman_to_arabic(split_roman(roman_num)))
+	# arab_total = sum_of_arab_nums(char_list)
+	print("The final total is: ", arab_total)
+
+if __name__=="__main__":
+	main()
+
--- skills2/skills2.py
+++ skills2/skills2.py
@@ -1,7 +1,12 @@
+
 string1 = "I do not like green eggs and ham."
 list1 = [2, 5, 12, 6, 1, -5, 8, 5, 6, -2, 2, 27]
 list2 = [-5, 6, 4, 8, 15, 16, 23, 42, 2, 7]
 words = ["I", "do", "not", "like", "green", "eggs", "and", "ham", "I", "do", "not", "like", "them", "San", "I", "am"]
+
+more_words = ['cat', 'dog', 'turtle', 'kangaroo']
+more_words2 = ['cat', 'turtle', 'fish', 'peacock', 'elephant', 'lion']
+
 
 # """
 # Write a function that takes a string and produces a dictionary with
@@ -25,28 +30,28 @@ words = ["I", "do", "not", "like", "green", "eggs", "and", "ham", "I", "do", "no
 
 	
 
-"""
-Given two lists, (without using the keyword 'in' or the method 'index')
-return a list of all common items shared between both lists
-"""
-full_list = []
-common_list = []
+# """
+# Given two lists, (without using the keyword 'in' or the method 'index')
+# return a list of all common items shared between both lists
+# """
+# full_list = []
+# common_list = []
 
-def common_items(list1, list2):
-    full_list = list1 + list2
-    for item in full_list:
-    	if full_list.count(item) > 1:
-    		common_list.append(item)
-    for item in common_list:
-    	if common_list.count(item) > 1:
-    		common_list.remove(item)
-    for item in common_list:
-    	if common_list.count(item) > 1:
-    		common_list.remove(item)
-    return common_list
+# def common_items(list1, list2):
+#     full_list = list1 + list2
+#     for item in full_list:
+#     	if full_list.count(item) > 1:
+#     		common_list.append(item)
+#     for item in common_list:
+#     	if common_list.count(item) > 1:
+#     		common_list.remove(item)
+#     for item in common_list:
+#     	if common_list.count(item) > 1:
+#     		common_list.remove(item)
+#     return common_list
 
-common_items_list = common_items(list1, list2)
-print common_items_list
+# common_items_list = common_items(list1, list2)
+# print common_items_list
 
     
 
@@ -58,51 +63,162 @@ print common_items_list
 # use a dictionary as part of your solution.
 # """
 # def common_items2(list1, list2):
-#     pass
+# 	common_dict = {}
+# 	common_list = []
+# 	for item in list1:
+# 		common_dict[item] = list2
+
+# 	for key, value in common_dict.iteritems():
+# 		for i in range(0, len(list2)):
+# 			if key == list2[i]:
+# 				common_list.append(list2[i])
+# 	return common_list
+
+# common_items = common_items2(list1, list2)
+# print common_items
 
 # """
 # Given a list of numbers, return list of number pairs that sum to zero
 # """
 # def sum_zero(list1):
-#     pass
+#     sum_dict = {}
+#     sum_list = []
+#     for item in list1:
+#     	sum_dict[item] = list1
+#     print sum_dict
+
+#     for k, v in sum_dict.iteritems():
+#     	for i in range(0, len(list1)):
+#     		if k == -1 * list1[i]:
+#     			sum_list.append((k, list1[i]))
+
+#     return sum_list
+
+
+# sum_zero_list = sum_zero(list1)
+# print sum_zero_list
+
+
 
 # """
 # Given a list of words, return a list of words with duplicates removed
 # """
 # def find_duplicates(words):
-#     pass
+# 	unique_list = []
+# 	duplicates_list = []
+# 	for word in words:
+# 		if word in unique_list:
+# 			if word in duplicates_list:
+# 				pass
+# 			else:
+# 				duplicates_list.append(word)
+# 		else:
+# 			unique_list.append(word)
 
+# 	return duplicates_list
+
+# duplicates = find_duplicates(words)
+# print duplicates
+   
 # """
 # Given a list of words, print the words in ascending order of length
 # Bonus: do it on a file instead of the list provided
 # Bonus: print the words in alphabetical order in ascending order of length
 # """
+
 # def word_length(words):
-#     pass
+# 	word_length_dict = {}
+# 	word_length_list = []
+# 	for word in words:
+# 		word_length_dict[word] = len(word)
 
-# """
-# Here's a table of English to Pirate translations
-# English     Pirate
+# 	for i in range(1,len(word_length_dict)):
+# 		for key, value in word_length_dict.iteritems():
+# 			if word_length_dict[key] == i:
+# 				word_length_list.append(key)
+# 		i += 1
 
-# sir         matey
-# hotel       fleabag inn
-# student     swabbie
-# boy         matey
-# madam       proud beauty
-# professor   foul blaggart
-# restaurant  galley
-# your        yer
-# excuse      arr
-# students    swabbies
-# are         be
-# lawyer      foul blaggart
-# the         th'
-# restroom    head
-# my          me
-# hello       avast
-# is          be
-# man         matey
+# 	return word_length_list
 
-# Write a program that asks the user to type in a sentence and then
-# print the sentece translated to pirate.
-# """
+# word_length_list = word_length(words)
+# print word_length_list
+
+
+"""
+Here's a table of English to Pirate translations
+English     Pirate
+
+sir         matey
+hotel       fleabag inn
+student     swabbie
+boy         matey
+madam       proud beauty
+professor   foul blaggart
+restaurant  galley
+your        yer
+excuse      arr
+students    swabbies
+are         be
+lawyer      foul blaggart
+the         th'
+restroom    head
+my          me
+hello       avast
+is          be
+man         matey
+
+Write a program that asks the user to type in a sentence and then
+print the sentece translated to pirate.
+"""
+def main():
+
+	input_sentence = raw_input("What would you like to translate? ")
+	word_list = []
+
+
+	def piratize(input_sentence):
+
+		input_sentence.split()
+		for i in range(0,):
+			if input_sentence[i] == "sir":
+				input_sentence[i] = "matey"
+			if input_sentence[i] == "hotel":
+				input_sentence[i] = "fleabag inn"
+			if input_sentence[i] == "student":
+				input_sentence[i] = "swabbie"
+			if input_sentence[i] == "boy":
+				input_sentence[i] = "matey"
+			if input_sentence[i] == "madam":
+				input_sentence[i] = "porud beauty"
+			if input_sentence[i] == "professor":
+				input_sentence[i] = "foul braggart"
+			if input_sentence[i] == "retaurant":
+				input_sentence[i] = "galley"
+			if input_sentence[i] == "your":
+				input_sentence[i] = "yer"
+			if input_sentence[i] == "are":
+				input_sentence[i] = "be"
+			if input_sentence[i] == "the":
+				input_sentence[i] = "th'"
+			if input_sentence[i] == "my":
+				input_sentence[i] = "me"
+			if input_sentence[i] == "is":
+				input_sentence[i] = "be"
+
+		print input_sentence
+		return input_sentence
+	translation = piratize(input_sentence)
+	print translation
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
+
+
+
+
+
